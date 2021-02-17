e784e12b0ca30ecd01a02c0b34bf38d413d8df49
Claudia Wang <claudiawang@utexas.edu>
2021-02-17 14:51:35 -0600

    with comments

--- extract_words.py
+++ extract_words.py
@@ -1,13 +1,16 @@
 # extract_words.py
 
+# imports
 import re
 import collections
 
+# open all files to read or append
 original = open("metamorphosis.txt", "r")
 allwords = open("allwords.txt", "a")
 uniqwords = open("uniquewords.txt", "a")
 wordfreq = open("wordfrequency.txt", "a")
 
+# all words to lowercase and remove all punctuation
 for x in original:
     x = x.lower()
     x = re.sub(r'[^\w\s]','',x)
@@ -17,6 +20,7 @@ allwords.close()
 
 allwords = open("allwords.txt", "r")
 
+# create dictionary to store keys (words) and values (count)
 uniq = {}
 
 for x in allwords:
@@ -27,12 +31,13 @@ for x in allwords:
         else:
             uniq[word] += 1
             
-            
+# write the keys from the uniq dictionary to uniquewords.txt          
 for k in uniq.keys():
     uniqwords.write(k + "\n")
     
 uniqwords.close()
 
+# tabulate the frequencies 
 freq = {}
 for v in uniq.values():
     if v not in freq:
@@ -40,9 +45,10 @@ for v in uniq.values():
     else:
         freq[v] += 1
         
-
-        
+# sort the frequencies from freq
 od = collections.OrderedDict(sorted(freq.items()))
+
+# write the sorted frequencies to wordfrequency.txt
 for k, v in od.items():
     wordfreq.write(str(k) + ": " + str(v) + "\n")
     
